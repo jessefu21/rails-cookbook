f4d5ba52b95e2f01f890b3311c40ad7d253b558a
Shanshan Fu <jesse.fu21@gmail.com>
2024-09-30 11:03:29 +0100

    validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :recipes, through: :bookmarks
 
+  validates :name, presence: true, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
-  has_many :categories
 
+  validates :name, presence: true, uniqueness: true
+  validates :descreption, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+  # inclusion: {in 0..10}
 end
