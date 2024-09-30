6c18e5197e9200bf01f49689b26ebf7c38500e49
Shanshan Fu <jesse.fu21@gmail.com>
2024-09-30 17:30:28 +0100

    created user journey

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,33 @@
 class BookmarksController < ApplicationController
+  before_action :set_category, only: [ :new, :create ]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.create(bookmark_params)
+    @bookmark.category = @category
+    if @bookmark.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to category_path(@bookmark.category)
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :recipe_id, :category_id)
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :categories, except: [ :edit, :update ]
-  resources :bookmarks
+  resources :categories, except: [ :edit, :update ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [ :destroy ]
+  root "categories#index"
 end
